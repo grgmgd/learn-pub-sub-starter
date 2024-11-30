21223915a9cba3aa5637abcccda9846b01116d61
George Maged <george.maged@payrails.com>
2024-11-30 14:32:57 +0200

    feat: added channel and exchange

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,44 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+	"os/signal"
+
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
+var CONNECTION_STRING = "amqp://guest:guest@localhost:5672/"
 
 func main() {
 	fmt.Println("Starting Peril server...")
+
+	conn, err := amqp.Dial(CONNECTION_STRING)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Connected to RabbitMQ")
+	defer conn.Close()
+
+	channel, err := conn.Channel()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Opened channel")
+	defer channel.Close()
+
+	pubsub.PublishJSON(channel,
+		routing.ExchangePerilDirect,
+		routing.PauseKey,
+		routing.PlayingState{IsPaused: true},
+	)
+
+	ch := make(chan os.Signal, 1)
+	signal.Notify(ch, os.Interrupt) // provide channel and signals to listen to, otherwise all signals will be relayed
+	<-ch                            // block until signal is received
+	fmt.Println("Received interrupt signal, exiting...")
 }
--- rabbit.sh
+++ rabbit.sh
@@ -5,9 +5,17 @@ case "$1" in
         echo "Starting RabbitMQ container..."
         docker run -d --rm --name rabbitmq -p 5672:5672 -p 15672:15672 rabbitmq:3.13-management
         ;;
+    start-stomp)
+        echo "Starting RabbitMQ container with STOMP plugin..."
+        docker run -d --rm --name rabbitmq_stomp -p 5672:5672 -p 15672:15672 -p 61613:61613 rabbitmq-stomp
+        ;;
     stop)
         echo "Stopping RabbitMQ container..."
         docker stop rabbitmq
+        ;;
+    stop-stomp)
+        echo "Stopping RabbitMQ container with STOMP plugin..."
+        docker stop rabbitmq_stomp
         ;;
     logs)
         echo "Fetching logs for RabbitMQ container..."
